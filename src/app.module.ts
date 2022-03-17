61df94b01b642525b20ec8c63b871d9ca1300967
Lucas Dias Coelho <ludico1959@gmail.com>
2022-03-17 14:34:13 -0300

    feat(docker): fix docker mysql database container problems related to connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './modules/user/user.module';
+import config from 'ormconfig';
 
 @Module({
-  imports: [
-    UsersModule,
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.MYSQL_HOST,
-      port: Number(process.env.MYSQL_PORT),
-      username: process.env.MYSQL_USER,
-      password: process.env.MYSQL_PASSWORD,
-      database: process.env.MYSQL_DB,
-      entities: ['dist/modules/user/infra/typeorm/entities/*.entity.js'],
-      migrations: ['dist/shared/database/migrations/*.js'],
-      synchronize: true,
-    }),
-  ],
+  imports: [UsersModule, TypeOrmModule.forRoot(config)],
   controllers: [],
   providers: [],
 })
