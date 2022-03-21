341f0be13b8e9a8a7bb3dbf5c5c6a5b53658407b
Lucas Dias Coelho <ludico1959@gmail.com>
2022-03-21 14:05:07 -0300

    feat(dto): add class validators to the create user dto

--- src/modules/user/dto/CreateUser.dto.ts
+++ src/modules/user/dto/CreateUser.dto.ts
@@ -1,17 +1,43 @@
-import { IsString, IsNotEmpty } from 'class-validator';
+import { IsString, IsNotEmpty, IsDate, IsArray } from 'class-validator';
 
 export default class CreateUserDTO {
   @IsString()
   @IsNotEmpty()
   login: string;
 
+  @IsString()
+  @IsNotEmpty()
   id: string;
+
+  @IsString()
+  @IsNotEmpty()
   node_id: string;
+
+  @IsString()
+  @IsNotEmpty()
   name: string;
+
+  @IsString()
+  @IsNotEmpty()
   html_url: string;
+
+  @IsString()
+  @IsNotEmpty()
   repos_url: string;
+
+  @IsDate()
+  @IsNotEmpty()
   updated_at: Date;
+
+  @IsDate()
+  @IsNotEmpty()
   created_at: Date;
+
+  @IsString()
+  @IsNotEmpty()
   email: string;
+
+  @IsArray()
+  @IsNotEmpty()
   repos: Array<string>;
 }
