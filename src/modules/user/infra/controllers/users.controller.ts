71dbb6c806dd4f9b68351d41dab644ba066a0625
Lucas Dias Coelho <ludico1959@gmail.com>
2022-03-25 10:27:10 -0300

    refactor(controller): update users controller

--- src/modules/user/infra/controllers/users.controller.ts
+++ src/modules/user/infra/controllers/users.controller.ts
@@ -1,13 +1,15 @@
 import { Controller, Get, HttpCode, Param } from '@nestjs/common';
-import { UsersService } from '../../usecases/CreateUser/CreateUser.service';
+import { ListUserByUsernameService } from '../../usecases/ListUserByUsernameService/ListUserByUsernameService';
 
 @Controller('/api/v1/users/')
 export class UsersController {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly listUsersByUsernameService: ListUserByUsernameService,
+  ) {}
 
   @Get(':login')
   @HttpCode(200)
   getUserByUsername(@Param('login') login: string): any {
-    return this.userService.getUserByUsername(login);
+    return this.listUsersByUsernameService.execute(login);
   }
 }
